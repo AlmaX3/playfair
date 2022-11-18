f51d9dc9a20eb27cada1ffb029329828b549ce16
vincze.daniel <vincze.daniel@28-08>
2022-11-18 15:19:27 +0100

    Add project files.

---
+++ playfair/PlayfairKodolo.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace playfair
+{
+    class PlayfairKodolo
+    {
+        public char[,] karakterMatrix { get; set; } = new char[5, 5];
+
+        public PlayfairKodolo(char[,] _karakterMatrix)
+        {
+            this.karakterMatrix = _karakterMatrix;
+        }
+
+        public void Kiiratas()
+        {
+            for (int i = 0; i < karakterMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < karakterMatrix.GetLength(1); j++)
+                {
+                    Console.Write(karakterMatrix[i, j]);
+                }
+                Console.WriteLine("");
+            }
+
+        }
+
+        public int SorIndex(char keresett)
+        {
+            for (int i = 0; i < karakterMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < karakterMatrix.GetLength(1); j++)
+                {
+                    if(keresett == karakterMatrix[i, j])
+                    {
+                        return i;
+                    }
+                }
+            }
+            return -1;
+        }
+
+        public int OszlopIndex(char keresett)
+        {
+            for (int i = 0; i < karakterMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < karakterMatrix.GetLength(1); j++)
+                {
+                    if (keresett == karakterMatrix[i, j])
+                    {
+                        return j;
+                    }
+                }
+            }
+            return -1;
+        }
+
+        public string KodolBetupar(string Betupar)
+        {
+            int betuSor1 = SorIndex(Betupar[0]);
+            int betuOszlo1 = OszlopIndex(Betupar[0]);
+            int betuSor2 = SorIndex(Betupar[1]);
+            int betuOszlo2 = OszlopIndex(Betupar[1]);
+            
+            if(betuOszlo1 + 1 >= 5 && betuSor1 == betuSor2)
+            {
+                betuOszlo1 = 0;
+                betuOszlo2++;
+
+                return $"{karakterMatrix[betuSor1, betuOszlo1]}{karakterMatrix[betuSor2, betuOszlo2]}";
+            }
+
+            if (betuOszlo2 + 1 >= 5 && betuSor1 == betuSor2)
+            {
+                betuOszlo2 = 0;
+                betuOszlo1++;
+
+                return $"{karakterMatrix[betuSor1, betuOszlo1]}{karakterMatrix[betuSor2, betuOszlo2]}";
+            }
+
+            if(betuOszlo1+1 == betuOszlo2 && betuSor1 == betuSor2)
+            {
+                betuOszlo1++;
+                betuOszlo2++;
+                return $"{karakterMatrix[betuSor1, betuOszlo1]}{karakterMatrix[betuSor2, betuOszlo2]}";
+            }
+
+            if (betuSor1 + 1 >= 5)
+            {
+                betuSor1 = 0;
+                betuSor2++;
+
+                return $"{karakterMatrix[betuSor1, betuOszlo1]}{karakterMatrix[betuSor2, betuOszlo2]}";
+            }
+
+            if (betuSor2 + 1 >= 5)
+            {
+                betuSor2 = 0;
+                betuSor1++;
+
+                return $"{karakterMatrix[betuSor1, betuOszlo1]}{karakterMatrix[betuSor2, betuOszlo2]}";
+            }
+
+            if (betuOszlo1 == betuOszlo2 && betuSor1+1 == betuSor2)
+            {
+                betuSor1++;
+                betuSor2++;
+                return $"{karakterMatrix[betuSor1, betuOszlo1]}{karakterMatrix[betuSor2, betuOszlo2]}";
+            }
+
+            if(betuOszlo1 != betuOszlo2 && betuSor1 != betuSor2)
+            {
+                return $"{karakterMatrix[betuSor1, karakterMatrix.GetLength(1) - (betuOszlo1 + 1)]}{karakterMatrix[betuSor2, karakterMatrix.GetLength(1) - (betuOszlo2 + 1)]}";
+            }
+
+            return $"{karakterMatrix[betuSor1, betuOszlo1]}{karakterMatrix[betuSor2, betuOszlo2]}";
+        }
+    }
+}
---
+++ playfair/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+
+namespace playfair
+{
+    internal class Program
+    {
+        static char[,] Feltoltes()
+        {
+            char[,] toSend = new char[5,5];
+            int idx = 0;
+            StreamReader sr = new StreamReader("kulcstabla.txt");
+            while(!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                char[] lineAsCharArray = line.ToCharArray();
+                for (int i = 0; i < toSend.GetLength(0); i++)
+                {
+                    toSend[idx, i] = lineAsCharArray[i];
+                }
+                idx++;
+            }
+            sr.Close();
+
+            return toSend;
+        }
+
+        static void Kereses(PlayfairKodolo playfairKodolo)
+        {
+            Console.Write("6. Feladat: Kérem adjon egy angol ÁBÉCÉ nagy betűjelét: ");
+            string Keresni = Console.ReadLine().ToUpper();
+            char HelyesKeresett = Keresni[0];
+
+            int sor = playfairKodolo.SorIndex(HelyesKeresett);
+
+            int oszlop = playfairKodolo.OszlopIndex(HelyesKeresett);
+            Console.WriteLine($"A keresett érték ({HelyesKeresett}) sor indexe: {sor}");
+            Console.WriteLine($"A keresett érték ({HelyesKeresett}) oszlop indexe: {oszlop}");
+        }
+        static void Main(string[] args)
+        {
+            char[,] karakterKodolo = Feltoltes();
+            PlayfairKodolo playfairKodolo = new PlayfairKodolo(karakterKodolo);
+            Kereses(playfairKodolo);
+            string AAAA = playfairKodolo.KodolBetupar("IH");
+            Console.WriteLine(AAAA);
+        }
+    }
+}
